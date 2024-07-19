24649a00d9ee7c8ca26e6b73e39682a02e5b4f7b
sayhusain <hishamalmosawii@gmail.com>
2024-07-19 21:26:01 +0300

    log in finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Login from './Components/Login';
 
 function App() {
+  const [logged, setLogged] = useState(false);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!logged? <Login auth={setLogged} /> : <h1> Logged In</h1>}
     </div>
   );
 }
