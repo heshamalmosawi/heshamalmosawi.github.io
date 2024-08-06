49ac2e06fb91577fa1ad6af07adb79673683a70b
sayhusain <hishamalmosawii@gmail.com>
2024-08-06 17:21:44 +0300

    final

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReceiveAudit from "./Receiveauditgraph";
 import SendAudit from "./SendAuditgraph";
-import TechGraph from "./technologiesradar";
-import SkillsGraph from "./skillsgraph";
 import LastFive from "./lastfive";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';import './graphs.css';
 import UserInfo from "./Userinfo";
@@ -13,7 +11,7 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 const Dashboard = ({auth}) => {
     let token = localStorage.getItem('token')
     // return <Hello/>
-    return (<> <div className="both-info"> <UserInfo/> <LastFive/> </div> <div className="piecharts"> <SendAudit/> <ReceiveAudit/> <SkillsGraph/> <TechGraph/> </div> <Logoutbtn auth={auth}/> </>)
+    return (<> <div className="both-info"> <UserInfo/> <LastFive/> </div> <div className="piecharts"> <SendAudit/> <ReceiveAudit/> </div> <Logoutbtn auth={auth}/> </>)
 }
 
 export default Dashboard
